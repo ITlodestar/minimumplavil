7021d1390c2bf19962e22437f4f002dcdf3669d6
skyhand727 <skyhand500@hotmail.com>
2024-03-20 11:39:20 -0400

    Update UserController.php

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -24,6 +24,7 @@ class UserController extends Controller
             'wallet' => 'required|unique:wallets,wallet',
             'country' => 'required',
         ]);
+        
         if ($validator->fails()) { 
             $response = [
                         'status' => 'false',
